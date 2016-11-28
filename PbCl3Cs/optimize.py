ed14d36498b663912b591800c26c41b0c0499bde
Henry Herbol <hherbol@gmail.com>
2016-11-28 12:16:06 -0500

    Fixed bug in which lj_coul only grabbed up to 6 parameters and not N*3 where N is the number of elements

--- PbCl3Cs/optimize.py
+++ PbCl3Cs/optimize.py
@@ -1,6 +1,6 @@
 import mcsmrff_opt
 
-LJ = [[2.1, -1.05, 1.0], [3.98892, 4.15625, 4.0], [0.1, 0.1, 0.1]]
+LJ = [[2.0, -1.0, 1.0], [3.98892, 4.15625, 4.0], [0.1, 0.1, 0.1]]
 three_body_interactions = ['Pb, Pb, Pb',
                            'Pb, Pb, Cl',
                            'Pb, Pb, Cs',
@@ -29,16 +29,19 @@ three_body_interactions = ['Pb, Pb, Pb',
                            'Cs, Cs, Cl',
                            'Cs, Cs, Cs']
 
-Pb_, Pb = 111, 907
-Cl_, Cl = 21, 344
-Cs_, Cs = 72, 352
+Pb = 907
+Cl = 344
+Cs = 352
 tersoff_atoms = [Pb, Cl, Cs]
 
 mcsmrff_opt.run_mcsmrff_optimizer(
-    n_sets=2,
+    n_sets=5,
     sim_name="test",
     lennard_jones=LJ,
     atom_list=three_body_interactions,
+    tersoff=None,
+    lj_coul=None,
+    constant_charge=True,
     training_set_pickle_path=(
         "/fs/home/hch54/"
         "Grad-MCSMRFF/PbCl3Cs/set2/"
